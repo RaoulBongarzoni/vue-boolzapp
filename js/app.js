48c8dcaa7efcfdb92494e89cdae25be891a4701f
RaoulBongarzoni <raoul.bongarzoni99@gmail.com>
2023-11-22 00:56:26 +0100

    first commit

---
+++ js/app.js
@@ -0,0 +1,28 @@
+const { createApp } = Vue
+
+
+
+
+
+
+createApp({
+
+    data(){
+            /* il mio utente */
+            /* lista contatti */
+                /* contatto singolo */
+                    /* [{nome, immagine, visibilità, messaggi scambiati:[{data,contenuto,inviato/ricevuto}]}] */
+            },
+    methods:{
+        /* funzione che mi genera lista contatti */
+        /* funzione che mi fa vedere tutti i messaggi in dinamica del contatto corrente */
+    },
+    mounted(){}
+
+
+
+
+
+
+
+}).mount('#app')
